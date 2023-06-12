50b8c8543da8d42dbf09756e722e1b6f540f8aaa
phyllscollege <136282836+phyllscollege@users.noreply.github.com>
2023-06-12 03:15:10 +0200

    Add files via upload
    website uploaded files

---
+++ main.js
@@ -0,0 +1,24 @@
+let search = document.querySelector('.search-box');
+
+document.querySelector('#search-icon').onclick = () =>{
+    search.classList.toggle('active');
+}
+let menu = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () =>{
+    menu.classList.toggle('active');
+    search.classList.remove('active');
+}
+//hide menu and search box on scroll
+window.onscroll = () => {
+    menu.classList.toggle('active');
+    search.classList.remove('active');
+
+}
+
+// header
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
